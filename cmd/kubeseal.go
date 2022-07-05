d8579f78b4fe8d7f175dcd755ced45158f3e4864
Marc Nguyen <nguyen_marc@live.fr>
2022-07-05 17:23:47 +0200

    Add a Kubeseal command (#8)
    Co-authored-by: Kimmo Lehto <klehto@mirantis.com>

---
+++ cmd/kubeseal.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+
+	"github.com/urfave/cli/v2"
+)
+
+var secretFileRegex = regexp.MustCompile(`^(.*)-secret.(yml|yaml).local$`)
+var kubesealCommand = &cli.Command{
+	Name:  "kubeseal",
+	Usage: "Kubeseal every '-secret.yaml.local' files recursively",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "controller-namespace",
+			Value:   "sealed-secrets",
+			Usage:   "The namespace where the sealed secrets controller resides.",
+			EnvVars: []string{"SEALED_SECRETS_CONTROLLER_NAMESPACE"},
+		},
+		&cli.StringFlag{
+			Name:  "controller-name",
+			Value: "sealed-secrets",
+			Usage: "The name of the sealed secrets controller.",
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		err := filepath.Walk(".",
+			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if info.IsDir() {
+					return nil
+				}
+				if res := secretFileRegex.FindStringSubmatch(path); len(res) == 3 {
+					sealedFilePath := fmt.Sprintf("%s-sealed-secret.%s", res[1], res[2])
+					fmt.Printf("Processing %s\n", path)
+					_, err := exec.Command(
+						"kubeseal",
+						"--controller-namespace",
+						ctx.String("controller-namespace"),
+						"--controller-name",
+						ctx.String("controller-name"),
+						"--format",
+						"yaml",
+						"--secret-file",
+						path,
+						"--sealed-secret-file",
+						sealedFilePath,
+					).Output()
+					if err != nil {
+						fmt.Printf("%v\n", err)
+					}
+				}
+				return nil
+			})
+		if err != nil {
+			log.Println(err)
+		}
+		return nil
+	},
+}
--- cmd/root.go
+++ cmd/root.go
@@ -28,6 +28,7 @@ var App = &cli.App{
 				configStatusCommand,
 			},
 		},
+		kubesealCommand,
 		completionCommand,
 	},
 	EnableBashCompletion: true,
