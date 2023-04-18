6e1051d85004f669ac789afdd2c995d7c707ce54
Frédéric Thomas <86131814+fredouric@users.noreply.github.com>
2023-04-18 02:04:34 +0200

    feat: ipmi-api support (#10)
    * feat: ipmi-api support
    
    * fix(ipmi cli): less verbose
    
    * fix(cmd): simplify Credential serialization
    
    - Better error handling
    - Closing body to avoid zombie
    
    * chore(ipmi): add more details on success
    
    * chore(ipmi): add some doc + do not handle 404
    
    ---------
    
    Co-authored-by: Nguyen Marc <nguyen_marc@live.fr>

---
+++ cmd/ipmi.go
@@ -0,0 +1,94 @@
+package cmd
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var credential = Credential{}
+var address string
+var ipmiCommand = &cli.Command{
+	Name:        "ipmi",
+	ArgsUsage:   "host action",
+	Usage:       "Manage compute nodes using ipmi-api",
+	Description: "Send action to IPMI API. Available actions: on, off, cycle, status, soft, reset.",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:        "user",
+			Destination: &credential.Username,
+			Required:    true,
+			Usage:       "IPMI user provided",
+			EnvVars:     []string{"IPMIUSER"},
+		},
+		&cli.StringFlag{
+			Name:        "password",
+			Destination: &credential.Password,
+			Required:    true,
+			Usage:       "IPMI password",
+			EnvVars:     []string{"IPMIPASS"},
+		},
+		&cli.StringFlag{
+			Name:        "address",
+			Destination: &address,
+			Usage:       "API address",
+			Value:       "https://ipmi.internal",
+			EnvVars:     []string{"IPMIADDRESS"},
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		if ctx.NArg() != 2 {
+			return errors.New("not enough arguments, use --help")
+		}
+
+		host := ctx.Args().Get(0)
+		action := ctx.Args().Get(1)
+
+		client := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+
+		requestPath := fmt.Sprintf("%s/host/%s/%s", address, host, action)
+
+		postData, err := json.Marshal(credential)
+		if err != nil {
+			return err
+		}
+
+		resp, err := client.Post(requestPath, "application/json", bytes.NewBuffer(postData))
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.WithError(err).Warn("ipmi response body couldn't be read")
+		}
+
+		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+			log.WithFields(log.Fields{
+				"status": resp.StatusCode,
+				"body":   string(b),
+			}).Error("ipmi API returned non-OK status code")
+			return errors.New("ipmi API returned non-OK status code")
+		}
+		log.Info(string(b))
+
+		return nil
+	},
+}
